f8b99bd1aacab8c748036c6367e8490b51b250e2
Sanjeev Kumar <sanjeevonline@gmail.com>
2012-03-25 19:14:29 +0530

    deleteing duplicates

--- gameoflife/src/State.java
+++
@@ -1,9 +0,0 @@
-/**
- * State of each Cell in the Universe is stored as an instance of this enum .
- * @author Sanjeev Kumar
- *
- */
-public enum State {
-    /** Represents state of a cell. */
-    ALIVE, DEAD;
-}
